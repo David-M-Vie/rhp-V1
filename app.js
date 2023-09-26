547d5537e1fc23d726f7039b290cc5e45b6e0915
Vie <the_ruesi@zoho.com>
2023-09-26 21:30:36 +0100

    ap date

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ const state = {
   endDate: {dte: null, isVerified: false},
   decisionDate: {dte: null, isVerified: false},
   additionalDays: {days: null, isVerified: false},
-  apStart: {dte: null, isVerified: false},
+  apEnd: {day: null, isVerified: false},
   sanctionLength: null
 }
 
@@ -106,7 +106,7 @@ document.querySelector('#btn-1').addEventListener("click", () => {
 
     if(errorMsg.length > 0) {
       const output = document.querySelector('#output-1');
-      let html = "<ul>"
+      let html = `<ul class="red-box">`
       html += `<span class="close-error-box">x</span>`
       for(let i = 0; i < errorMsg.length; i++) {
         html += `<li>${errorMsg[i]}</li>`
@@ -126,7 +126,7 @@ document.querySelector('#btn-1').addEventListener("click", () => {
       state.sanctionLength = totalDays;
       const output = document.querySelector('#output-1')
       output.innerHTML = `
-        Sanction applies for ${state.sanctionLength} days
+        <p class="blue-box">Sanction applies for ${state.sanctionLength} days</p>
       `
     }
 
@@ -138,84 +138,48 @@ document.querySelector('#btn-1').addEventListener("click", () => {
 
 
 /* ===================================================
- SECTION 2 CALCULATE DECISION DATE, AND AP START DATE
+ SECTION 2 CALCULATE DECISION DATE, AND AP END DATE
 ====================================================== */
-document.querySelector('#decisionDate').addEventListener("keyup", (e) => {
-  state.decisionDate.dte = e.target.value;
-  state.decisionDate.isVerified = false;
+// document.querySelector('#decisionDate').addEventListener("keyup", (e) => {
+//   state.decisionDate.dte = e.target.value;
+//   state.decisionDate.isVerified = false;
+// })
+
+
+// AP End Date - update state when input is updated.
+document.querySelector('#apEnd').addEventListener('click', (e) => {
+  state.apEnd.day = Number(e.target.value);
+  state.apEnd.isVerified = false;
 })
 
-document.querySelector('#apStart').addEventListener('click', (e) => {
-  state.apStart.days = Number(e.target.value);
-  state.apStart.isVerified = false;
+document.querySelector('#apEnd').addEventListener('keyup', (e) => {
+  state.apEnd.day = Number(e.target.value);
+  state.apEnd.isVerified = false;
 })
 
-
+// When the calculate button-2 is clicked,  validate the ap start date.
 document.querySelector('#btn-2').addEventListener("click", () => {
 
- // set an errors array
- const errorMsg = [];
+  const apEnd = document.querySelector('#apEnd').value;
+  const errorMsg = [];
 
- if(state.decisionDate.isVerified && state.apStart.isVerified ) {
-   scrollDown();
- }else {
-   // use the input to attempt to create a valid JS Date obj
-   const startDate = createDate(state.startDate.dte);
-   if(startDate === "Invalid Date") {
-     errorMsg.push("Date of failure is not a valid date");
-     state.startDate.isVerified = false;
-   }else {
-     // set the JS Date object on state
-     state.startDate.dte = startDate;
-     state.startDate.isVerified = true;
-   }
+  if(state.apEnd.isVerified) {
+    scrollDown();
+  }else {
+  
+    if(!apEnd) {
+      errorMsg.push('Assessment Period start date can\'t be blank')
+    }else if(apEnd > 31 || apEnd < 1){
+      errorMsg.push('The AP end date can\'t be less than 1 or greater than 31');
+    }
+    // WORKING HERE !!!!!!!!!!!!!!!!!!!!!!!
+    // Perhaps AP end should be a DATE OBJ, consider changing back to a date!
+  } 
 
-   const endDate = createDate(state.endDate.dte);
-   if(endDate === "Invalid Date") {
-     errorMsg.push("Date sanction lifted is not a valid date");
-     state.endDate.isVerified = false;
-
-   }else {
-     state.endDate.dte = endDate;
-     state.endDate.isVerified = true;
-   }
-
-   if(state.additionalDays.days === 0 || state.additionalDays.days === null) {
-     errorMsg.push("No additional days selected, 7 or 14.");
-     state.additionalDays.isVerified = false
-   }else {
-     state.additionalDays.isVerified = true
-   }
+});
 
 
-   if(errorMsg.length > 0) {
-     const output = document.querySelector('#output-1');
-     let html = "<ul>"
-     html += `<span class="close-error-box">x</span>`
-     for(let i = 0; i < errorMsg.length; i++) {
-       html += `<li>${errorMsg[i]}</li>`
-     }
-     html += "</ul>"
-     output.innerHTML = html;
-     document.querySelector('.close-error-box').addEventListener("click", () => {
-       output.innerHTML = ""
-     })
-   }else {
-     document.querySelector('#btn-1').textContent="Next"
-     document.querySelector('#btn-1').classList.replace('calc', 'next')
-     document.querySelector('#output-1').innerHTML = ""
 
-     // Calculate the difference between start and end days and add on the additional days
-     const totalDays = getTotalDays(state.startDate.dte, state.endDate.dte, state.additionalDays.days)
-     const output = document.querySelector('#output-1')
-     output.innerHTML = `
-       Sanction applies for ${totalDays} days
-     `
-   }
-
- }   
-
-})
 
  
 /*
@@ -226,9 +190,7 @@ document.querySelector('#btn-2').addEventListener("click", () => {
 const getTotalDays = (startDate, endDate, addDays) => {
   const oneDay = 24 * 60 * 60 * 1000; // hours*minutes*seconds*milliseconds
 
-  console.log(addDays)
-
-const totalDays = Math.round(Math.abs((endDate - startDate) / oneDay));
+  const totalDays = Math.round(Math.abs((endDate - startDate) / oneDay));
 
   return totalDays + addDays
 }
@@ -237,13 +199,7 @@ const totalDays = Math.round(Math.abs((endDate - startDate) / oneDay));
 
 
 /* SCROLLING SECTION NAVIGATION */
-// document.querySelector('#btn-2').addEventListener("click", () => {
-//   scrollDown();
-// });
 
-document.querySelector('#calc').addEventListener("click", () => {
-  scrollDown();
-});
 
 // document.querySelector('#startAgain').addEventListener("click", () => {
 //   scrollTo({
